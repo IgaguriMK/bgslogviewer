6e6d0540064a78d6c0cefd2a3fd98fec928ba995
Igaguri <igagurimk@gmail.com>
2018-09-07 23:11:47 +0900

    Add access logging to file (JSON)

--- bgslogviewer.go
+++ bgslogviewer.go
@@ -1,28 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"time"
 
 	"github.com/IgaguriMK/bgslogviewer/controller"
 	"github.com/gin-gonic/gin"
 )
 
-const (
-	systemNameLimit = 64
-)
-
-const (
-	cacheLocalMin = 120 * time.Second
-	cacheLocalMax = 60 * time.Minute
-	cacheCdnMin   = 5 * time.Minute
-	cacheCdnMax   = 6 * time.Hour
-)
+var logCh chan AccessLog
 
 func main() {
+	startLogSaver()
+
 	time.Sleep(3 * time.Second)
 
-	r := gin.Default()
+	e := gin.Default()
+
+	r := e.Use(accessLogger)
 
 	r.StaticFile("/", "./static/main.html")
 	r.StaticFile("/index.html", "./static/main.html")
@@ -45,8 +42,64 @@ func main() {
 
 	r.StaticFile("/robots.txt", "./static/misc/robots.txt")
 
-	err := r.Run(":8080")
+	err := e.Run(":8080")
 	if err != nil {
 		log.Fatal("[FATAL] Can't execute gin server: ", err)
+	}
+}
+
+type AccessLog struct {
+	Date      string `json:"date"`
+	Method    string `json:"method"`
+	Path      string `json:"path"`
+	Code      int64  `json:"code"`
+	From      string `json:"from"`
+	UserAgent string `json:"useragent"`
+	Duration  int64  `json:"dur_ms"`
+}
+
+func accessLogger(c *gin.Context) {
+	start := time.Now()
+
+	c.Next()
+
+	dur := time.Since(start)
+
+	logCh <- AccessLog{
+		Date:      start.Format(time.RFC3339),
+		Method:    c.Request.Method,
+		Path:      c.Request.URL.Path,
+		Code:      int64(c.Writer.Status()),
+		From:      c.ClientIP(),
+		UserAgent: c.Request.UserAgent(),
+		Duration:  int64(dur / time.Millisecond),
+	}
+}
+
+func startLogSaver() {
+	err := os.MkdirAll("./log", 0744)
+	if err != nil {
+		log.Fatal("alert: can't create log direcrory: ", err)
+	}
+
+	logCh = make(chan AccessLog, 8)
+	go func() {
+		for l := range logCh {
+			saveLog(l)
+		}
+	}()
+}
+
+func saveLog(l AccessLog) {
+	f, err := os.OpenFile("./log/access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("error: can't open access.log: ", err)
+		return
+	}
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(l)
+	if err != nil {
+		log.Println("error: access log save error:", err)
 	}
 }
