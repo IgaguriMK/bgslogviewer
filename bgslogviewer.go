59ce707ef8eb822566db7b65b7a0d6784a6b6416
Igaguri <igagurimk@gmail.com>
2018-09-07 21:15:40 +0900

    不正な文字種の星系名を排除

--- bgslogviewer.go
+++ bgslogviewer.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"log"
-	"net/url"
 	"time"
 
+	"github.com/IgaguriMK/bgslogviewer/controller"
 	"github.com/gin-gonic/gin"
-
-	"github.com/IgaguriMK/bgslogviewer/apiCaller"
-	"github.com/IgaguriMK/bgslogviewer/config"
-	"github.com/IgaguriMK/bgslogviewer/prof"
-	"github.com/IgaguriMK/bgslogviewer/view"
 )
 
 const (
@@ -35,7 +27,7 @@ func main() {
 	r.StaticFile("/", "./static/main.html")
 	r.StaticFile("/index.html", "./static/main.html")
 
-	r.GET("/system", statPage)
+	r.GET("/system", controller.StatPage)
 
 	r.Static("/static/css", "./static/css")
 	r.Static("/static/img", "./static/img")
@@ -57,131 +49,4 @@ func main() {
 	if err != nil {
 		log.Fatal("[FATAL] Can't execute gin server: ", err)
 	}
-}
-
-func statPage(c *gin.Context) {
-	pf := prof.NewProfiler()
-	pf.Start("validation")
-
-	systemName, ok := c.GetQuery("q")
-	if !ok {
-		c.String(400, "Invalid Url")
-		return
-	}
-
-	if systemName == "" {
-		c.Redirect(301, "/")
-		return
-	}
-
-	pf.AddParam("q", systemName)
-
-	commonHeader(c)
-
-	if systemName == "" || len(systemName) > systemNameLimit {
-		c.String(404, "Invalid query")
-		pf.End(404)
-		return
-	}
-
-	pf.Mark("fetchAPI")
-	v, stat, err := apiCaller.FetchFactions(systemName)
-	if err != nil {
-		c.String(500, "Internal error")
-		pf.End(500)
-		log.Println("[ERROR] fetching data error: ", err)
-		return
-	}
-
-	switch stat {
-	case apiCaller.Timeout:
-		c.String(503, "Too many access")
-		pf.End(503)
-		return
-	case apiCaller.Invalid:
-		c.String(404, "Invalid request")
-		pf.End(404)
-		return
-	}
-
-	if systemName != v.Name {
-		params := url.Values{}
-		params.Add("q", v.Name)
-		u := "/system?" + params.Encode()
-
-		c.Redirect(301, u)
-		return
-	}
-
-	pf.Mark("template")
-	body := new(bytes.Buffer)
-
-	err = view.System(body, v)
-	if err != nil {
-		c.String(500, "Internal error")
-		pf.End(500)
-		log.Fatal("[FATAL] Execute template: ", err)
-		return
-	}
-
-	c.Status(200)
-
-	ll, cl := cachesLen()
-	c.Header("Cache-Control", fmt.Sprintf("max-age=%d, s-maxage=%d", ll, cl))
-
-	pf.Mark("send")
-	_, err = io.Copy(c.Writer, body)
-	if err != nil {
-		pf.End(-1)
-		log.Println("[INFO] error while sending data: ", err)
-	}
-
-	pf.End(200)
-}
-
-func commonHeader(c *gin.Context) {
-	c.Header("X-XSS=Protection", "1; mode=block")
-	c.Header("X-Frame-Options", "DENY")
-	c.Header("X-Content-Type-Options", "nosniff")
-	c.Header("Content-Security-Policy", "default-src 'self'")
-}
-
-func cachesLen() (localCache int, cdnCache int) {
-	now := time.Now()
-
-	nextUpdate := time.Date(
-		now.Year(),
-		now.Month(),
-		now.Day(),
-		config.BgsUpdate,
-		0,
-		0,
-		0,
-		time.UTC,
-	)
-
-	if nextUpdate.Before(now) {
-		nextUpdate = nextUpdate.Add(24 * time.Hour)
-	}
-
-	prevUpdate := nextUpdate.Add(-24 * time.Hour)
-
-	// 直前のBGS更新から近いときは最短キャッシュ
-	if now.Add(-3 * time.Hour).Before(prevUpdate) {
-		return int(cacheLocalMin.Seconds()), int(cacheCdnMin.Seconds())
-	}
-
-	// 次のBGS更新まで間近
-	if now.Add(time.Hour).After(nextUpdate) {
-		return int(cacheLocalMin.Seconds()), int(cacheCdnMin.Seconds())
-	}
-
-	// BGS更新付近ではないが、最大キャッシュでは長すぎる
-	if now.Add(cacheCdnMax + cacheCdnMin + time.Hour).After(nextUpdate) {
-		d := nextUpdate.Sub(now) - time.Hour
-		return int(d.Seconds()), int(d.Seconds())
-	}
-
-	// 次のBGS更新まで余裕がある
-	return int(cacheLocalMax.Seconds()), int(cacheCdnMax.Seconds())
 }
